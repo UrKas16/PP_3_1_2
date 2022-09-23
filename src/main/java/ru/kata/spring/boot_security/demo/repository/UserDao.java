82a330070d10d9ae82ca0477a4969b6540dc48cf
Yurii <kasyurvik568@gmail.com>
2022-09-23 20:29:45 +0300

    merge with pp_3_1_1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserDao.java
@@ -0,0 +1,17 @@
+package ru.kata.spring.boot_security.demo.repository;
+
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+
+public interface UserDao {
+    void saveUser(User user);
+    User getUserById(long id);
+
+    void updateUser(User user);
+
+    List<User> getAllUsers();
+
+    void deleteUserById(long id);
+}
